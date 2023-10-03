8e17d4d73285fbe76872eeeb1423f73fbe036d13
itachi1010 <onahben977@gmail.com>
2023-10-03 00:46:31 -1100

    i added gunicorn to the requirements

--- youtube_downloader/settings.py
+++ youtube_downloader/settings.py
@@ -86,12 +86,24 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CORS_ALLOW_ALL_ORIGINS = True
 
 #CORS_ALLOWED_ORIGINS = ['https://127.0.0.1', 'https://127.0.0.1:52498', 'http://localhost:8000', "http://localhost:58757", 'https://localhost']
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
